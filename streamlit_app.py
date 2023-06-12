a25feeb32acbe6d3331021d32966cb97deb51356
chakrabartinabab <42760446+chakrabartinabab@users.noreply.github.com>
2023-06-12 19:30:30 +0530

    test debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,4 @@ st.header('Lunch Menu')
 st.text('Luchi')
 st.text('Aloo dum')
 st.text('ghugni')
-
 st.title("My smoothie")
-
-# adding a new dataframe to the application page
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-# Display the table on the page.
-st.dataframe(my_fruit_list)
