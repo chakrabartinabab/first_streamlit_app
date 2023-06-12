d7ac62a2520818938daa06950b76255ed6e23b27
chakrabartinabab <42760446+chakrabartinabab@users.noreply.github.com>
2023-06-12 19:23:39 +0530

    trying to restart the app by changing the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,12 @@ st.text('Luchi')
 st.text('Aloo dum')
 st.text('ghugni')
 
+st.title("my smoothie")
+
 # adding a new dataframe to the application page
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-st.dataframe(my_fruit_list)
+
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
