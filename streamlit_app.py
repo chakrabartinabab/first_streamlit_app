7cc4bf16058470d003b9bfd64b53f0c64e7ebff4
chakrabartinabab <42760446+chakrabartinabab@users.noreply.github.com>
2023-06-12 19:08:36 +0530

    adding a new data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ st.header('Lunch Menu')
 st.text('Luchi')
 st.text('Aloo dum')
 st.text('ghugni')
+
+# adding a new dataframe to the application page
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
