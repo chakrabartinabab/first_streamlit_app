1c9ee0094a274a7da99a6d5c42cb04548de7093b
chakrabartinabab <42760446+chakrabartinabab@users.noreply.github.com>
2023-06-13 17:11:03 +0530

    reorganizing imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from urllib.error import URLerror
+import pandas
+import requests
+import snowflake.connector
+
 
 st.title('My Parents New healthy diner')
 st.header('Lunch Menu')
@@ -7,7 +12,7 @@ st.text('Aloo dum')
 st.text('ghugni')
 st.title("My smoothie")
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected=st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Orange','Cherries'])
@@ -19,7 +24,7 @@ st.header("Fruityvice Fruit Advice!")
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #st.text(fruityvice_response.json())--commenting it out to remove from output screen
 
@@ -30,7 +35,7 @@ st.dataframe(fruityvice_normalized)
 
 st.stop()
 
-import snowflake.connector
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
